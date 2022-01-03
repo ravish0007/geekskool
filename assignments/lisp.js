152dce1fc6607acc2bdb00767ecb75f4bb11e88d
ravish0007 <ravish.nfs@gmail.com>
2022-01-03 12:37:22 +0530

    Update lisp.js, implement lambda expression

--- assignments/lisp.js
+++ assignments/lisp.js
@@ -1,196 +1,202 @@
 // Exceptions
-function throwSyntaxError(error) {
-    throw error;
+function throwSyntaxError (error) {
+  throw error
 }
-
 
 // Types
-let Symbol_ = token => String(token);   //try inheriting String
+const Symbol_ = token => String(token)
 
 // Parsing: parse, tokenize, and read_from_tokens
-function tokenize(chars) {
-     // Convert a string of characters into a list of tokens.
-    return chars.replace(/\(/g, '( ').replace(/\)/g, ' ) ').split(' ').filter(Boolean);
+function tokenize (chars) {
+  // Convert a string of characters into a list of tokens.
+  return chars.replace(/\(/g, '( ').replace(/\)/g, ' ) ').split(' ').filter(Boolean)
 }
 
-function parse(program) {
-    // Read a Scheme expression from a string.
-    return read_from_tokens(tokenize(program));
+function parse (program) {
+  // Read a Scheme expression from a string.
+  return readFromTokens(tokenize(program))
 }
 
-function read_from_tokens(tokens) {
-    // Read an expression from a sequence of tokens.
-    if (tokens.length == 0) {
-        thowSyntaxError('unexpected EOF');
+function readFromTokens (tokens) {
+  // Read an expression from a sequence of tokens.
+  if (tokens.length == 0) {
+    throwSyntaxError('unexpected EOF')
+  }
+
+  const token = tokens.shift()
+
+  if (token === '(') {
+    const L = []
+    while (tokens[0] !== ')') {
+      L.push(readFromTokens(tokens))
     }
-
-    token = tokens.shift();
-
-    if (token == '(') {
-        let L = [];
-        while (tokens[0] != ')') {
-            L.push(read_from_tokens(tokens));
-        }
-        tokens.shift();  // pop off ')'
-        return L;
-    }
-
-    else if(token == ')') {
-        throwSyntaxError('unexpected )');
-    }
-
-    else return atom(token);
+    tokens.shift() // pop off ')'
+    return L
+  } else if (token === ')') {
+    throwSyntaxError('unexpected )')
+  } else return atom(token)
 }
 
-
-
-function atom(token) {
-    // Numbers become numbers; every other token is a symbol.
-        let num = Number(token);
-        if(!Number.isNaN(num)) return num;
-        return Symbol_(token);
+function atom (token) {
+  // Numbers become numbers; every other token is a symbol.
+  const num = Number(token)
+  if (!Number.isNaN(num)) return num
+  return Symbol_(token)
 }
-
 
 // Environments
-function standard_env() {
-    
-    // An environment with some Scheme standard procedures.
+function standardEnv () {
+  // An environment with some Scheme standard procedures.
 
-    let env = new Env();
+  const env = new Env()
 
-    env.update(Math);  //sin, cos, sqrt, pi...
+  env.update(Math) // sin, cos, sqrt, pi...
 
-    env.update({
-        '+' : (a, b) => a + b,
-        '-' : (a, b) => a - b,
-        '*' : (a, b) => a * b,
-        '/' : (a, b) => Math.floor(a/b), 
-        '>' : (a, b) => a > b,
-        '<' : (a, b) => a < b,
-        '>=' : (a, b) => a >= b,
-        '<=' : (a, b) => a <= b,
-        '=' : (a, b) => a == b,
-        // 'abs' : abs,   exists in Math
-        'append' : (l1, l2) => [...l1, ...l2],  
-        // 'apply' : apply, Not defined, seems like reducer
-        'begin' : (...x) => x[x.length - 1],
-        'car' : x => x[0],
-        'cdr' : x => x.slice(1),
-        'cons' : (x,y) => [x, y],  // more resolution (obj1 is car, obj2 is cdr)
-        'eq?' : (a,b) => a == b,
-        'equal?' : (a, b) => a === b,
-        'length' : x => x.length, 
-        'list' : (...x) => x, 
-        'list?' : x => Array.isArray(x), 
-        'map' : x => x.map,        // more resolution
-        // 'max' : max,  exists in Math
-        // 'min' : min,  exists in Math
-        'not' : x => !x,
-        'null?' : x => x.length == 0, 
-        'number?' : x => (typeof x ==  'number' && !isNaN(x)),
-        'procedure?' : x => typeof x == 'function',
-        // 'round' : round, exists in Math
-        'symbol?' : x => typeof x == 'string',            // better way of identifying as symbol
-    })
+  env.update({
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => Math.floor(a / b),
+    '>': (a, b) => a > b,
+    '<': (a, b) => a < b,
+    '>=': (a, b) => a >= b,
+    '<=': (a, b) => a <= b,
+    '=': (a, b) => a == b,
+    // 'abs' : abs,   exists in Math
+    append: (l1, l2) => [...l1, ...l2],
+    apply: x => x.reduce, // TODO: fix
+    begin: (...x) => x[x.length - 1], // TODO: fix
+    car: x => x[0],
+    cdr: x => x.slice(1),
+    cons: (x, y) => [x, y], // more resolution (obj1 is car, obj2 is cdr)
+    'eq?': (a, b) => a == b,
+    'equal?': (a, b) => a === b,
+    length: x => x.length,
+    list: (...x) => x,
+    'list?': x => Array.isArray(x),
+    map: x => x.map, // more resolution
+    // 'max' : max,  exists in Math
+    // 'min' : min,  exists in Math
+    not: x => !x,
+    'null?': x => x.length === 0,
+    'number?': x => (typeof x === 'number' && !isNaN(x)),
+    'procedure?': x => typeof x === 'function',
+    // 'round' : round, exists in Math
+    'symbol?': x => typeof x === 'string' // better way of identifying as symbol
+  })
 
-    return env;
+  return env
 }
 
 class Env {
+  update (env, outer = null) {
+    Object.getOwnPropertyNames(env).map(property => this[property] = env[property])
+    this.outer = outer
+  }
 
-    update(env, outer=null) {
-      Object.getOwnPropertyNames(env).map(property => this[property] = env[property]);
-      this['outer'] = outer;
-    }
-
-    find(var_) {
-        return this.hasOwnProperty(var_) ? this : this.outer.find(var_);
-    }
-
+  find (var_) {
+    return this.hasOwnProperty(var_) ? this : this.outer.find(var_)
+  }
 }
 
+const globalEnv = standardEnv()
 
-let global_env = standard_env();
-
-// eval
-function eval(x, env=global_env) {
-    // Evaluate an expression in an environment.
-
-    if(typeof x == 'string')        // variable reference    // better way of identifying Symbol
-        return env[x];
-
-    else if(typeof x == 'number')      // constant number
-        return x;                
-
-    else if(x[0] == 'if') {               // conditional
-        let [_, test, conseq, alt] = x;
-        let exp = eval(test, env) ? conseq : alt;
-        return eval(exp, env);
+// Procedures
+function procedure (params, body, env) {
+  // A user-defined Scheme procedure.
+  return function (...args) {
+    const newEnv = new Env()
+    const empty = {}
+    for (let i = 0; i < args.length; i++) {
+      empty[params[i]] = Number(args[i])
     }
+    newEnv.update(empty, env)
+    return evaluate(body, newEnv)
+  }
+}
 
-    else if(x[0] == 'define') {           // definition
-        let [_, symbol, exp] = x;
-        env[symbol] = eval(exp, env);
-    }
+// eval  rename(eval, evaluate)  remove else ifs
+function evaluate (x, env = globalEnv) {
+  // Evaluate an expression in an environment.
 
-    else {                           // procedure call
-        proc = eval(x[0], env);
-        args = x.slice(1).map(arg => eval(arg, env));
-        return proc(...args);
-    }
+  if (typeof x === 'string') // variable reference
+  { return env.find(x)[x] }
+
+  if (typeof x === 'number') // constant number    //may be elimante due to literal
+  { return x }
+
+  if (!Array.isArray(x)) { // constant literal     //might not required
+    return x
+  }
+
+  if (x[0] == 'quote') { // (quote exp)
+    const [_, exp] = x
+    return exp
+  }
+
+  if (x[0] == 'if') { // conditional
+    const [_, test, conseq, alt] = x
+    const exp = evaluate(test, env) ? conseq : alt
+    return evaluate(exp, env)
+  }
+
+  if (x[0] == 'define') { // definition
+    const [_, symbol, exp] = x
+    env[symbol] = evaluate(exp, env)
+    return // env[symbol]   // fixes begin procedure NaN
+  }
+
+  if (x[0] == 'set!') { // (set! var exp)
+    const [_, var_, exp] = x // might not define for new ones but defines for already defined
+    env.find(var_)[var_] = evaluate(exp, env)
+    return
+  }
+
+  if (x[0] == 'lambda') { // (lambda (var...) body)
+    const [_, parms, body] = x
+    return procedure(parms, body, env)
+  }
+
+  const proc = evaluate(x[0], env)
+  const args = x.slice(1).map(arg => evaluate(arg, env))
+  return proc(...args)
 }
 
 // Interaction: A REPL
 const readline = require('readline')
-const rl =  readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 })
 
-function repl() {
-    rl.question("lis.js> ", function(input) {
+function repl () {
+  rl.question('lis.js> ', function (input) {
+    input = input.trim()
 
-        input = input.trim()
-
-        if (input == "(exit)"){
-            rl.close();
-        } else {
-            let val = eval(parse(input));
-            console.log(val);
-            repl();
-        }
-    });
+    if (input == '(debug)') {
+      console.log(globalEnv)
+      repl()
+    } else if (input == '(exit)') {
+      rl.close()
+    } else {
+      const val = evaluate(parse(input))
+      if (val != undefined) {
+        console.log(lispstr(val))
+      }
+      repl()
+    }
+  })
 }
 
+// Test repl
 repl()
 
+function lispstr (exp) {
+  // "Convert a JavaScript object back into a Lisp-readable string.
 
-// def lispstr(exp):
-//     // "Convert a Python object back into a Lisp-readable string."
-//     if isinstance(exp, List):
-//         return '(' + ' '.join(map(lispstr, exp)) + ')'
-//     else:
-//         return str(exp)
-
-
-
-// Test
-let x = '((hello x x))'
-let r = '(+ 2 2)'
-let program = "(begin (define r 10) (* pi (* r r)))"
-let pprogram = "(* 3.14 (* 10 10))"
-let proogram = "(define r 10)"
-
-// console.log(typeof parse(r))
-// console.log(eval(parse(program)))
-// console.log(global_env)
-// console.log(typeof atom('1'))
-// console.log(typeof atom('('))
-// console.log(typeof atom('begin'))
-// console.log(typeof atom('1.451'))
-// console.log(parse(r));
-// console.log(eval(parse(r)), 2);
-// console.log(global_env);
-
-
+  if (Array.isArray(exp)) {
+    return '(' + exp.map(x => lispstr(x)).join(' ') + ')'
+  } else {
+    return String(exp)
+  }
+}
